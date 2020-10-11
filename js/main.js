db4b44b4d28cfd7f53b9bb1c21fe3e90ebad57ef
Ben Graham <benben1510@gmail.com>
2020-10-11 15:23:23 +0300

    update ignore

--- js/main.js
+++ js/main.js
@@ -1,1 +1,57 @@
 console.log('Starting up');
+
+
+
+function onInit() {
+    // createProjects()
+    renderProjects()
+}
+
+
+onInit()
+
+function renderProjects() {
+ console.log('checking');  
+var strHtml = gProjects.map(function (project) {
+    return `
+    <div class="col-md-4 col-sm-6 portfolio-item">
+    <a id ="${project.id}class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+      <div class="portfolio-hover">
+        <div class="portfolio-hover-content">
+          <i class="fa fa-plus fa-3x"></i>
+        </div>
+      </div>
+      <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
+    </a>
+    <div class="portfolio-caption">
+      <h4>${project.name}</h4>
+      <p class="text-muted">${project.title}</p>
+    </div>
+  </div>
+    `
+})
+document.querySelector('.projects-container').innerHTML = strHtml.join('');
+
+}
+
+
+function createProjects(id) {
+    var project = getProjById(id)
+    var strHTML = ` 
+    strHTML += <div class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
+          </a>
+          <div class="portfolio-caption">
+            <h4>${project.name}</h4>
+            <p class="text-muted">${project.title}</p>
+          </div>
+        </div>
+        `
+        document.querySelector('.projects-container').innerHTML = strHTML;
+    }
---
+++ js/proj-service.js
@@ -0,0 +1,67 @@
+'use strict'
+
+var gProjects = [
+    {
+     id: "pop-balloon",
+     name: "Pop Balloon",
+     title: "Better pop them fast",
+     desc: "try to catch them all before they fly away",
+     url: "projs/pop-balloon",
+     publishedAt: 1448693940000,
+     labels: ["Matrixes", "keyboard events"],
+    },
+    {
+     id: "minesweeper",
+     name: "Minesweeper",
+     title: "Watch out for the mines",
+     desc: "try to flag them all before you blow up",
+     url: "projs/minesweeper",
+     publishedAt: 1448693940000,
+     labels: ["Matrixes", "keyboard events"],
+    },
+    {
+     id: "book-shop",
+     name: "Book Shop",
+     title: "Come read something good",
+     desc: "Lets see what books are in",
+     url: "projs/book-shop",
+     publishedAt: 1448693940000,
+     labels: ["Matrixes", "keyboard events"],
+    },
+    {
+     id: "chess",
+     name: "Chess",
+     title: "Check-Mate?",
+     desc: "try to win the best",
+     url: "projs/chess",
+     publishedAt: 1448693940000,
+     labels: ["Matrixes", "keyboard events"],
+    },
+    {
+     id: "ball-game",
+     name: "Catch the Ball",
+     title: "Run fast and catch them all",
+     desc: "try to catch them all before they fly away",
+     url: "projs/ball-game",
+     publishedAt: 1448693940000,
+     labels: ["Matrixes", "keyboard events"],
+    },
+    {
+     id: "touch-nums",
+     name: "Touch the Numbers",
+     title: "Keep order",
+     desc: "try to catch them all before they fly away",
+     url: "projs/touch-nums",
+     publishedAt: 1448693940000,
+     labels: ["Matrixes", "keyboard events"],
+    },
+    ]
+    
+    function getProjById(projId) {
+        var project = gProjects.find(function (project) {
+            return projId === project.id
+        })
+        return project
+    }
+
+    
